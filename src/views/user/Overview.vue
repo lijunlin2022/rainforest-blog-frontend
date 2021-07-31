6945c18f19cc74bffa2f9d2e03565beea49097f2
a-layfolk <1808150553@qq.com>
2021-07-31 22:02:04 +0800

    [feat] add the profile

--- src/views/user/Overview.vue
+++ src/views/user/Overview.vue
@@ -1,58 +1,74 @@
 <template>
   <div class="blog-content">
-    <!------- 笔记本 ------->
-    <div class="tips">
-      <span>Popular Notebooks</span>
-    </div>
-    <div class="notebooks">
-      <blog-directory
-        v-for="item in notebookArray"
-        :key="item.id"
-        @click="$utils.changeRoute('/notebookDetail', item.id)"
-      >
-        <template v-slot:name>
-          {{ item.name }}
-        </template>
-        <template v-slot:description>
-          {{ item.description }}
-        </template>
-      </blog-directory>
-    </div>
+    <div class="center">
+      <!-- 简介 -->
+      <div class="title">
+        <span>Brief Introduction</span>
+      </div>
 
-    <!------- 文章列表 ------->
-    <div class="tips">
-      <span>Recent Articles</span>
-    </div>
-    <div class="articles">
-      <blog-file v-for="item in articleArray" :key="item.id">
-        <template v-slot:title>
-          {{ item.title }}
-        </template>
-        <template v-slot:abstract>
-          {{ item.abstract }}
-        </template>
-        <template v-slot:createdTime>
-          {{ $utils.getYearMonthDay(item.created_time) }}
-        </template>
-        <template v-slot:updatedTime>
-          {{ $utils.getYearMonthDay(item.updated_time) }}
-        </template>
-        <template v-slot:link>
-          <a @click="$utils.changeRoute('/detail', item.id)">Read more ></a>
-        </template>
-      </blog-file>
-    </div>
+      <div class="profile">
+        <img
+          src="https://gitee.com/Li-Jun-Lin/figure/raw/master/avater/trees.png"
+        />
+        <div>
+          <v-md-preview :text="profileContent"></v-md-preview>
+        </div>
+      </div>
 
-    <!------- 下一页 ------->
-    <div class="paging">
-      <a-button v-if="hasNext" @click="handleNextBtn">Next</a-button>
-      <a-button v-else>No more...</a-button>
+      <!------- 笔记本 ------->
+      <div class="title">
+        <span>Popular Notebooks</span>
+      </div>
+      <div class="notebooks">
+        <blog-directory
+          v-for="item in notebookArray"
+          :key="item.id"
+          @click="$utils.changeRoute('/notebookDetail', item.id)"
+        >
+          <template v-slot:name>
+            {{ item.name }}
+          </template>
+          <template v-slot:description>
+            {{ item.description }}
+          </template>
+        </blog-directory>
+      </div>
+
+      <!------- 文章列表 ------->
+      <div class="title">
+        <span>Recent Articles</span>
+      </div>
+      <div class="articles">
+        <blog-file v-for="item in articleArray" :key="item.id">
+          <template v-slot:title>
+            {{ item.title }}
+          </template>
+          <template v-slot:abstract>
+            {{ item.abstract }}
+          </template>
+          <template v-slot:createdTime>
+            {{ $utils.getYearMonthDay(item.created_time) }}
+          </template>
+          <template v-slot:updatedTime>
+            {{ $utils.getYearMonthDay(item.updated_time) }}
+          </template>
+          <template v-slot:link>
+            <a @click="$utils.changeRoute('/detail', item.id)">Read more ></a>
+          </template>
+        </blog-file>
+      </div>
+
+      <!------- 下一页 ------->
+      <div class="paging">
+        <a-button v-if="hasNext" @click="handleNextBtn">Next</a-button>
+        <a-button v-else>No more...</a-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getBlogsList } from "@/api/blogs.js";
+import { getBlogsList, getBlogDetail } from "@/api/blogs.js";
 import { getNotebooksList } from "@/api/notebooks.js";
 import BlogDirectory from "@/components/directory/BlogDirectory.vue";
 import BlogFile from "@/components/file/BlogFile.vue";
@@ -68,44 +84,39 @@ export default {
       current: 0,
       size: 5,
       hasNext: true,
+      profileContent: "",
       notebookArray: [],
       articleArray: [],
     };
   },
   methods: {
-    handleGetHotBlogList() {
-      getNotebooksList(0, 6).then((result) => {
-        let array = result.data.data.map((item) => {
-          return this.$utils.htmlDecodeObject(item);
-        });
-        this.notebookArray = array;
-      });
-    },
-    handleGetListByPage() {
-      getBlogsList(null, this.current, this.size).then((result) => {
-        let array = result.data.data.map((item) => {
-          return this.$utils.htmlDecodeObject(item);
-        });
-        this.articleArray = array;
-      });
-    },
-    handleNextBtn() {
+    async handleNextBtn() {
       this.current++;
-      getBlogsList(null, this.current, this.size).then((result) => {
-        let array = result.data.data.map((item) => {
-          return this.$utils.htmlDecodeObject(item);
-        });
-
-        this.articleArray.push(...array);
-        if (result.data.data.length < this.size) {
-          this.hasNext = false;
-        }
+      const res = await getBlogsList(null, this.current, this.size);
+      let array = res.data.data.map((item) => {
+        return this.$utils.htmlDecodeObject(item);
       });
+      this.articleArray.push(...array);
+      if (res.data.data.length < this.size) {
+        this.hasNext = false;
+      }
     },
   },
-  created() {
-    this.handleGetHotBlogList();
-    this.handleGetListByPage();
+  async created() {
+    try {
+      const profile = await getBlogDetail(null, 1, "PROFILE");
+      const notebooks = await getNotebooksList(0, 6);
+      const articles = await getBlogsList(null, this.current, this.size);
+      this.profileContent = this.$utils.htmlDecode(profile.data.data.content);
+      this.notebookArray = notebooks.data.data.map((item) => {
+        return this.$utils.htmlDecodeObject(item);
+      });
+      this.articleArray = articles.data.data.map((item) => {
+        return this.$utils.htmlDecodeObject(item);
+      });
+    } catch (error) {
+      console.error("网络请求失败");
+    }
   },
 };
 </script>
@@ -115,25 +126,41 @@ export default {
   box-sizing: border-box;
   min-height: calc(100vh - 60px);
 }
-.tips {
-  margin: 78px auto 18px;
+.center {
+  border-top: 1px solid #fff;
   max-width: 900px;
+  margin: auto;
+}
+.title {
+  margin: 30px 0;
   padding: 0 16px;
   color: var(--secondary-color);
 }
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.profile > img {
+  width: 100px;
+}
+.profile > div {
+  padding: 20px;
+  font-size: 14px;
+}
+@media screen and (min-width: 992px) {
+  .profile {
+    flex-direction: row;
+  }
+  .profile > img {
+    width: 200px;
+  }
+}
 .notebooks {
-  margin: 30px auto 8px;
-  max-width: 900px;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   font-size: 14px;
-}
-.articles {
-  margin: 30px auto;
-  max-width: 900px;
-  /* display: flex;
-  flex-direction: column; */
 }
 .paging {
   display: flex;
